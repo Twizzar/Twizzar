c268080f53afe38071b466967c1d74670f0d5af7
Tobias Bollinger <tobias.bollinger@vi-sit.com>
2023-06-15 13:53:07 +0200

    removed the partial in the test class

--- examples/PotionDeliveryService/PotionDeliveryService.Tests/DeliveryServiceTests.cs
+++ examples/PotionDeliveryService/PotionDeliveryService.Tests/DeliveryServiceTests.cs
@@ -7,7 +7,7 @@ using PotionDeliveryService.Interfaces;
 namespace PotionDeliveryService.Tests;
 
 [TestFixture]
-public partial class DeliveryServiceTests
+public class DeliveryServiceTests
 {
     [Test]
     public void Deliver_sends_package_to_destination_over_parcelService()
